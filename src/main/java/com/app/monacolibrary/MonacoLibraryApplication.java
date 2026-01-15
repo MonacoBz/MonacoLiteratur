afbc970fd0d3b5059ef23cfaa956d42f6af95ae6
MonacoBz <gerardossj2@hotmail.com>
2026-01-15 17:39:35 -0600

    first commit

---
+++ src/main/java/com/app/monacolibrary/MonacoLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.app.monacolibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonacoLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonacoLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/monacolibrary/MonacoLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.monacolibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonacoLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
