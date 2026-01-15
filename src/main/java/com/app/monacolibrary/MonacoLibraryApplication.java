c97e9fd38c677d6058db63e6a3cd8893ff67c078
MonacoBz <gerardossj2@hotmail.com>
2026-01-15 17:41:10 -0600

    algo

--- src/main/java/com/app/monacolibrary/MonacoLibraryApplication.java
+++ src/main/java/com/app/monacolibrary/MonacoLibraryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MonacoLibraryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MonacoLibraryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MonacoLibraryApplication.class, args);
+    }
 
 }
