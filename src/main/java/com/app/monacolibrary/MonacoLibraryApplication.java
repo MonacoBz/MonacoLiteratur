05f211d6b2cb72cedd3e54cdf2054fd0ee0acca6
MonacoBz <gerardossj2@hotmail.com>
2026-01-16 15:08:33 -0600

    Se termino la creación de la solicitud HTTP

--- src/main/java/com/app/monacolibrary/MonacoLibraryApplication.java
+++ src/main/java/com/app/monacolibrary/MonacoLibraryApplication.java
@@ -1,13 +1,21 @@
 package com.app.monacolibrary;
 
+import com.app.monacolibrary.service.HttpApi;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MonacoLibraryApplication {
+public class MonacoLibraryApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(MonacoLibraryApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        HttpApi api = new HttpApi();
+        api.realizaSolicitud();
+
+    }
 }
---
+++ src/main/java/com/app/monacolibrary/service/HttpApi.java
@@ -0,0 +1,31 @@
+package com.app.monacolibrary.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class HttpApi {
+
+    public String realizaSolicitud(String url){
+        HttpClient client = HttpClient.newBuilder()
+                .followRedirects(HttpClient.Redirect.ALWAYS)
+                .build();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = null;
+
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        String json = response.body();
+        return json;
+    }
+}
